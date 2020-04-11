63260788a6055c77c0cecd811833bbfeb26ff685
Dinan Rangga Maulana <dinanranggamaulana@gmail.com>
2020-04-11 21:56:38 +0700

    feat: add controller and views from each models

---
+++ Controllers/OrganizersController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EventOrganizer.Data;
+using EventOrganizer.Models;
+
+namespace EventOrganizer.Controllers
+{
+    public class OrganizersController : Controller
+    {
+        private readonly EventOrganizerDbContext _context;
+
+        public OrganizersController(EventOrganizerDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Organizers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Organizer.ToListAsync());
+        }
+
+        // GET: Organizers/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var organizer = await _context.Organizer
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (organizer == null)
+            {
+                return NotFound();
+            }
+
+            return View(organizer);
+        }
+
+        // GET: Organizers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Organizers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,ImageLocation,IsActive,CreatedAt,UpdatedAt")] Organizer organizer)
+        {
+            if (ModelState.IsValid)
+            {
+                organizer.Id = Guid.NewGuid();
+                _context.Add(organizer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(organizer);
+        }
+
+        // GET: Organizers/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var organizer = await _context.Organizer.FindAsync(id);
+            if (organizer == null)
+            {
+                return NotFound();
+            }
+            return View(organizer);
+        }
+
+        // POST: Organizers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,ImageLocation,IsActive,CreatedAt,UpdatedAt")] Organizer organizer)
+        {
+            if (id != organizer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(organizer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrganizerExists(organizer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(organizer);
+        }
+
+        // GET: Organizers/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var organizer = await _context.Organizer
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (organizer == null)
+            {
+                return NotFound();
+            }
+
+            return View(organizer);
+        }
+
+        // POST: Organizers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var organizer = await _context.Organizer.FindAsync(id);
+            _context.Organizer.Remove(organizer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrganizerExists(Guid id)
+        {
+            return _context.Organizer.Any(e => e.Id == id);
+        }
+    }
+}
